da8701e9de3050f4b9868ae35c977402547ce64f
EfraSino13 <37081831+EfraSino13@users.noreply.github.com>
2026-02-11 12:41:48 -0600

    nueva version de la app

---
+++ src/src/components/TextFooter.tsx
@@ -0,0 +1,29 @@
+import { Playfair_Display } from "next/font/google";
+import Link from "next/link";
+
+const playfairDisplay = Playfair_Display({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export default function TextFooter() {
+  return (
+    <>
+      {/* Left Text */}
+      <h1
+        className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
+      >
+        <span className="text-cyan-400">Has match</span> <br /> en nuestras fotos 
+      </h1>
+
+      {/* Right Text */}
+      <h1
+        className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
+      >
+        para revelar <br /> <span className="text-cyan-400">la sorpresa</span>
+      </h1>
+
+      {/* Copyright */}
+    </>
+  );
+}
